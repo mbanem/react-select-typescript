b0aee39691ebaab8286a1c2678333e31edbb45a5
mbanem <mili.comrunner@gmail.com>
2020-12-26 11:02:50 +0100

    deleted test files

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { render } from 'react-dom';
 
 import { App } from './Components/App';
-// import { Filter } from './Components/Filter';
-// import { CustomClearIndicator } from './example';
-
 import data from './Models/data.json';
+
 render(<App {...data} />, document.getElementById('root'));
