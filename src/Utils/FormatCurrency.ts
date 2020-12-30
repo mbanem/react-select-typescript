8e45d193229ac58a9a401c34fa8d50091d9a7bd2
mbanem <mili.comrunner@gmail.com>
2020-12-30 18:32:19 +0100

    cart with no Proceed

--- src/Utils/FormatCurrency.ts
+++ src/Utils/FormatCurrency.ts
@@ -1,9 +1,10 @@
 export const formatCurrency = (num: number) => {
 	const dec = ['.00', '0', ''];
-	const m = num.toLocaleString().match(/(\d+)\.?(\d*)?/);
+	const number = Math.round(num * 100) / 100;
+	const m = number.toLocaleString().match(/(\d+)\.?(\d*)?/);
 	if (m) {
 		const length = m[2] ? m[2].length : 0;
-		return `$${num}${dec[length]}`;
+		return `$${number}${dec[length]}`;
 	}
 	throw new Error(`formatCurrency: invalid entry ${num}`);
 };
